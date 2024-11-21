852e541e472ec73213d5761e54ae1a3c6d8f5d90
ijaamm <ijammusyaf@gmail.com>
2024-11-21 17:24:38 +0700

    membuat data class mahasiswa

---
+++ app/src/main/java/com/example/praktikum7/model/Mahasiswa.kt
@@ -0,0 +1,9 @@
+package com.example.praktikum7.model
+
+data class Mahasiswa(
+    val nama : String = "",
+    val gender : String = "",
+    val alamat : String = "",
+    val email : String = "",
+    val noTelpon : String = ""
+)
