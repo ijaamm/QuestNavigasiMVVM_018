9d588e791ed7ef2c529712ce6ba57c8e620743c1
ijaamm <ijammusyaf@gmail.com>
2024-11-21 17:24:11 +0700

    membuat object list gender

---
+++ app/src/main/java/com/example/praktikum7/model/ListSex.kt
@@ -0,0 +1,10 @@
+package com.example.praktikum7.model
+
+import com.example.praktikum7.R
+
+object ListGender {
+    val listGender = listOf(
+        R.string.laki,
+        R.string.wanita
+    )
+}
