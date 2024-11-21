7289da76a6933ae4283a23af41cfb25485c620f9
ijaamm <ijammusyaf@gmail.com>
2024-11-21 16:31:48 +0700

    membuat halaman untuk form

--- app/src/main/java/com/example/praktikum7/Navigation.kt
+++ app/src/main/java/com/example/praktikum7/Navigation.kt
@@ -1,7 +1,10 @@
 package com.example.praktikum7
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
 
 enum class Halaman{
                   Form, Data
@@ -11,4 +14,11 @@ fun Navigasi(
     modifier: Modifier = Modifier,
     viewModel: MahasiswaViewModel = viewModel(),
     navHost: NavHostController = rememberNavController()
-){}
+){
+    composable(route = Halaman.Form.name) {
+        val konteks = LocalContext.current
+        FormMahasiswaView(
+
+            listGender = ListGender.listGender.map { id -> konteks.getString(id)
+        )
+    }
