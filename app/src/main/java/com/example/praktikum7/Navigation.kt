fc07d1d46134de24eae924d65a066e6ad1234594
ijaamm <ijammusyaf@gmail.com>
2024-11-21 16:23:49 +0700

    membuat composable dan fun navigasi

--- app/src/main/java/com/example/praktikum7/Navigation.kt
+++ app/src/main/java/com/example/praktikum7/Navigation.kt
@@ -1,5 +1,14 @@
 package com.example.praktikum7
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
 enum class Halaman{
                   Form, Data
 }
+@Composable
+fun Navigasi(
+    modifier: Modifier = Modifier,
+    viewModel: MahasiswaViewModel = viewModel(),
+    navHost: NavHostController = rememberNavController()
+){}
